87e7a99f241676b3dd2b3cf6fa532741ee7a25a8
TheOryZ <kadirrturan@gmail.com>
2022-04-11 23:59:17 +0300

    Added jwt-service.go
    Added jwt-service.go

---
+++ pkg/services/jwt-service.go
@@ -0,0 +1,63 @@
+package services
+
+import (
+	"os"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+)
+
+//JWTService interface
+type JWTService interface {
+	GenerateToken(userID string) string
+	ValidateToken(token string) (*jwt.Token, error)
+}
+
+type jwtCustomClaims struct {
+	UserID string `json:"user_id"`
+	jwt.StandardClaims
+}
+
+type jwtService struct {
+	secretKey string
+	issuer    string
+}
+
+func NewJWTService() JWTService {
+	return &jwtService{
+		secretKey: getSecretKey(),
+		issuer:    "http://localhost:8080",
+	}
+}
+
+func getSecretKey() string {
+	secretKey := os.Getenv("JWT_SECRET")
+	if secretKey == "" {
+		secretKey = "JohnDoeSecrets"
+	}
+	return secretKey
+}
+
+func (j *jwtService) GenerateToken(userID string) string {
+	claims := &jwtCustomClaims{
+		userID,
+		jwt.StandardClaims{
+			ExpiresAt: time.Now().AddDate(0, 0, 1).Unix(),
+			Issuer:    j.issuer,
+			IssuedAt:  time.Now().Unix(),
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	t, err := token.SignedString([]byte(j.secretKey))
+	if err != nil {
+		panic(err)
+	}
+	return t
+}
+
+func (j *jwtService) ValidateToken(token string) (*jwt.Token, error) {
+	return jwt.ParseWithClaims(token, &jwtCustomClaims{}, func(token *jwt.Token) (interface{}, error) {
+		return []byte(j.secretKey), nil
+	})
+}
