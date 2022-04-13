5e808a639059652453556104dc143c7cb46c2ba4
Kadir Turan <kadirrturan@gmail.com>
2022-04-13 13:45:17 +0300

    Added role-service.go and category-service.go
    Added role-service.go and category-service.go

---
+++ pkg/services/category-service.go
@@ -0,0 +1,199 @@
+package services
+
+import (
+	"time"
+
+	"github.com/Picus-Security-Golang-Bootcamp/bitirme-projesi-TheOryZ/internal/store/domain/category"
+	"github.com/Picus-Security-Golang-Bootcamp/bitirme-projesi-TheOryZ/pkg/dtos"
+	"github.com/gofrs/uuid"
+)
+
+//CategoryService is an interface for CategoryService
+type CategoryService interface {
+	Create(model dtos.CategoryCreateDTO) (dtos.CategoryListDTO, error)
+	Update(model dtos.CategoryUpdateDTO) (dtos.CategoryListDTO, error)
+	Delete(model dtos.CategoryUpdateDTO) error
+	DeleteByID(id uuid.UUID) error
+	FindAll() ([]dtos.CategoryListDTO, error)
+	FindAllWithPagination(page int, limit int) ([]dtos.CategoryListDTO, error)
+	CountAll() (int64, error)
+	FindByID(id uuid.UUID) (dtos.CategoryListDTO, error)
+	FindByName(name string) (dtos.CategoryListDTO, error)
+	SearchByName(ss string) ([]dtos.CategoryListDTO, error)
+	FindAllWithProducts() ([]dtos.CategoryListDTO, error) //TODO:
+}
+
+//categoryService is an implementation of CategoryService
+type categoryService struct {
+	categoryRepository category.ICategoryRepository
+}
+
+//NewCategoryService is a constructor for CategoryService
+func NewCategoryService(categoryRepository category.ICategoryRepository) CategoryService {
+	return &categoryService{categoryRepository: categoryRepository}
+}
+
+//Create a new category
+func (s *categoryService) Create(model dtos.CategoryCreateDTO) (dtos.CategoryListDTO, error) {
+	listModel := dtos.CategoryListDTO{}
+	categoryModel := category.Category{
+		Name:      model.Name,
+		CreatedAt: time.Now().Format("2006-01-02 15:04:05"),
+		UpdatedAt: time.Now().Format("2006-01-02 15:04:05"),
+	}
+	err := s.categoryRepository.Create(&categoryModel)
+	if err != nil {
+		return listModel, err
+	}
+	listModel = dtos.CategoryListDTO{
+		ID:   categoryModel.ID,
+		Name: categoryModel.Name,
+	}
+	return listModel, nil
+}
+
+//Update a category
+func (s *categoryService) Update(model dtos.CategoryUpdateDTO) (dtos.CategoryListDTO, error) {
+	listModel := dtos.CategoryListDTO{}
+	categoryModel := category.Category{
+		ID:        model.ID,
+		Name:      model.Name,
+		UpdatedAt: time.Now().Format("2006-01-02 15:04:05"),
+	}
+	err := s.categoryRepository.Update(&categoryModel)
+	if err != nil {
+		return listModel, err
+	}
+	listModel = dtos.CategoryListDTO{
+		ID:   categoryModel.ID,
+		Name: categoryModel.Name,
+	}
+	return listModel, nil
+}
+
+//Delete a category
+func (s *categoryService) Delete(model dtos.CategoryUpdateDTO) error {
+	categoryModel, err := s.categoryRepository.FindByID(model.ID)
+	if err != nil {
+		return err
+	}
+	err = s.categoryRepository.Delete(categoryModel)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+//DeleteByID a category
+func (s *categoryService) DeleteByID(id uuid.UUID) error {
+	categoryModel, err := s.categoryRepository.FindByID(id)
+	if err != nil {
+		return err
+	}
+	err = s.categoryRepository.Delete(categoryModel)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+//FindAll category
+func (s *categoryService) FindAll() ([]dtos.CategoryListDTO, error) {
+	listModel := []dtos.CategoryListDTO{}
+	categoryModels, err := s.categoryRepository.FindAll()
+	if err != nil {
+		return listModel, err
+	}
+	for _, categoryModel := range categoryModels {
+		listModel = append(listModel, dtos.CategoryListDTO{
+			ID:   categoryModel.ID,
+			Name: categoryModel.Name,
+		})
+	}
+	return listModel, nil
+}
+
+//FindAllWithPagination category
+func (s *categoryService) FindAllWithPagination(page int, limit int) ([]dtos.CategoryListDTO, error) {
+	listModel := []dtos.CategoryListDTO{}
+	categoryModels, err := s.categoryRepository.FindAllWithPagination(page, limit)
+	if err != nil {
+		return listModel, err
+	}
+	for _, categoryModel := range categoryModels {
+		listModel = append(listModel, dtos.CategoryListDTO{
+			ID:   categoryModel.ID,
+			Name: categoryModel.Name,
+		})
+	}
+	return listModel, nil
+}
+
+//CountAll category
+func (s *categoryService) CountAll() (int64, error) {
+	count, err := s.categoryRepository.CountAll()
+	if err != nil {
+		return 0, err
+	}
+	return count, nil
+}
+
+//FindByID category
+func (s *categoryService) FindByID(id uuid.UUID) (dtos.CategoryListDTO, error) {
+	listModel := dtos.CategoryListDTO{}
+	categoryModel, err := s.categoryRepository.FindByID(id)
+	if err != nil {
+		return listModel, err
+	}
+	listModel = dtos.CategoryListDTO{
+		ID:   categoryModel.ID,
+		Name: categoryModel.Name,
+	}
+	return listModel, nil
+}
+
+//FindByName category
+func (s *categoryService) FindByName(name string) (dtos.CategoryListDTO, error) {
+	listModel := dtos.CategoryListDTO{}
+	categoryModel, err := s.categoryRepository.FindByName(name)
+	if err != nil {
+		return listModel, err
+	}
+	listModel = dtos.CategoryListDTO{
+		ID:   categoryModel.ID,
+		Name: categoryModel.Name,
+	}
+	return listModel, nil
+}
+
+//SearchByName category
+func (s *categoryService) SearchByName(ss string) ([]dtos.CategoryListDTO, error) {
+	listModel := []dtos.CategoryListDTO{}
+	categoryModels, err := s.categoryRepository.SearchByName(ss)
+	if err != nil {
+		return listModel, err
+	}
+	for _, categoryModel := range categoryModels {
+		listModel = append(listModel, dtos.CategoryListDTO{
+			ID:   categoryModel.ID,
+			Name: categoryModel.Name,
+		})
+	}
+	return listModel, nil
+}
+
+//FindAllWithProducts category
+func (s *categoryService) FindAllWithProducts() ([]dtos.CategoryListDTO, error) {
+	listModel := []dtos.CategoryListDTO{}
+	categoryModels, err := s.categoryRepository.FindAllWithProducts()
+	if err != nil {
+		return listModel, err
+	}
+	for _, categoryModel := range categoryModels {
+		listModel = append(listModel, dtos.CategoryListDTO{
+			ID:   categoryModel.ID,
+			Name: categoryModel.Name,
+		})
+	}
+	return listModel, nil
+}
---
+++ pkg/services/role-service.go
@@ -0,0 +1,138 @@
+package services
+
+import (
+	"time"
+
+	"github.com/Picus-Security-Golang-Bootcamp/bitirme-projesi-TheOryZ/internal/store/domain/role"
+	"github.com/Picus-Security-Golang-Bootcamp/bitirme-projesi-TheOryZ/pkg/dtos"
+	"github.com/gofrs/uuid"
+)
+
+//RoleService is an interface for RoleService
+type RoleService interface {
+	Create(model dtos.RoleCreateDTO) (dtos.RoleListDTO, error)
+	Update(model dtos.RoleUpdateDTO) (dtos.RoleListDTO, error)
+	Delete(model dtos.RoleUpdateDTO) error
+	DeleteByID(id uuid.UUID) error
+	FindAll() ([]dtos.RoleListDTO, error)
+	FindByID(id uuid.UUID) (dtos.RoleListDTO, error)
+	FindByName(name string) (dtos.RoleListDTO, error)
+}
+
+//roleService is an implementation of RoleService
+type roleService struct {
+	roleRepository role.IRoleRepository
+}
+
+//NewRoleService is a constructor for RoleService
+func NewRoleService(roleRepository role.IRoleRepository) RoleService {
+	return &roleService{roleRepository: roleRepository}
+}
+
+//Create a new role
+func (s *roleService) Create(model dtos.RoleCreateDTO) (dtos.RoleListDTO, error) {
+	listModel := dtos.RoleListDTO{}
+	roleModel := role.Role{
+		Name:      model.Name,
+		CreatedAt: time.Now().Format("2006-01-02 15:04:05"),
+		UpdatedAt: time.Now().Format("2006-01-02 15:04:05"),
+	}
+	err := s.roleRepository.Create(&roleModel)
+	if err != nil {
+		return listModel, err
+	}
+	listModel = dtos.RoleListDTO{
+		ID:   roleModel.ID,
+		Name: roleModel.Name,
+	}
+	return listModel, nil
+}
+
+//Update a role
+func (s *roleService) Update(model dtos.RoleUpdateDTO) (dtos.RoleListDTO, error) {
+	listModel := dtos.RoleListDTO{}
+	roleModel := role.Role{
+		ID:        model.ID,
+		Name:      model.Name,
+		UpdatedAt: time.Now().Format("2006-01-02 15:04:05"),
+	}
+	err := s.roleRepository.Update(&roleModel)
+	if err != nil {
+		return listModel, err
+	}
+	listModel = dtos.RoleListDTO{
+		ID:   roleModel.ID,
+		Name: roleModel.Name,
+	}
+	return listModel, nil
+}
+
+//Delete a role
+func (s *roleService) Delete(model dtos.RoleUpdateDTO) error {
+	roleModel, err := s.roleRepository.FindByID(model.ID)
+	if err != nil {
+		return err
+	}
+	err = s.roleRepository.Delete(roleModel)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+//DeleteByID a role
+func (s *roleService) DeleteByID(id uuid.UUID) error {
+	_, err := s.roleRepository.FindByID(id)
+	if err != nil {
+		return err
+	}
+	err = s.roleRepository.DeleteByID(id)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+//FindAll roles
+func (s *roleService) FindAll() ([]dtos.RoleListDTO, error) {
+	listModel := []dtos.RoleListDTO{}
+	roles, err := s.roleRepository.FindAll()
+	if err != nil {
+		return listModel, err
+	}
+	for _, role := range roles {
+		listModel = append(listModel, dtos.RoleListDTO{
+			ID:   role.ID,
+			Name: role.Name,
+		})
+	}
+	return listModel, nil
+}
+
+//FindByID a role
+func (s *roleService) FindByID(id uuid.UUID) (dtos.RoleListDTO, error) {
+	listModel := dtos.RoleListDTO{}
+	roleModel, err := s.roleRepository.FindByID(id)
+	if err != nil {
+		return listModel, err
+	}
+	listModel = dtos.RoleListDTO{
+		ID:   roleModel.ID,
+		Name: roleModel.Name,
+	}
+	return listModel, nil
+}
+
+//FindByName a role
+func (s *roleService) FindByName(name string) (dtos.RoleListDTO, error) {
+	listModel := dtos.RoleListDTO{}
+	roleModel, err := s.roleRepository.FindByName(name)
+	if err != nil {
+		return listModel, err
+	}
+	listModel = dtos.RoleListDTO{
+		ID:   roleModel.ID,
+		Name: roleModel.Name,
+	}
+	return listModel, nil
+}
